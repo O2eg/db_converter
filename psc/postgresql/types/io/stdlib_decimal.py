55414dae853af0a037c3e47ee32247c16fb5397a
root <oleg.ispu@yandex.ru>
2020-07-20 23:20:17 +0300

    fixed case: "select 0::numeric"

--- psc/postgresql/types/io/stdlib_decimal.py
+++ psc/postgresql/types/io/stdlib_decimal.py
@@ -144,6 +144,8 @@ numeric_signs = {
 
 def numeric_unpack(x, unpack = lib.numeric_unpack):
 	header, digits = unpack(x)
+	if header == (0, 0, 0, 0) and len(digits) == 0:
+		digits = (0,)
 	npad = (header[3] - ((header[0] - (header[1] + 1)) * 4))
 	return Decimal((
 		numeric_signs.get(header[2], header[2]),
